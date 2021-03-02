c5c4fc5c91279ddf86f859afd646d99b0ae07a7b
IanPearce <ian.pearce.home@gmail.com>
2021-03-02 09:28:45 +0000

    Add files via upload

---
+++ src/DataContracts/BaseModules.tsx
@@ -0,0 +1,61 @@
+
+
+/*ADD TO THIS FILE*/
+export class ExternalApiCall {
+    /// <summary>
+    /// This is the Base Entity Id of the object with a setting, that holds the setting for the auth object
+    /// it can be either, Agency, Branch, Brand or Person, Determined by the External Provider Settings
+    /// </summary>
+    public AuthId?:number
+    
+    /// <summary>
+    /// GET,PUT,POST,PATCH,DELETE
+    /// </summary>
+    public Method: string;
+ 
+ 
+ 
+    /// <summary>
+    /// QueryString Appened to the baseUrl stored for the external provider
+    /// </summary>
+    public  QueryString: string
+ 
+    /// <summary>
+    /// The serialised data contract (if any) being sent the other api 
+    /// </summary>
+    public Data?: string
+
+    /// <summary>
+    /// Defaults to 10 if not supplied
+    /// </summary>
+    public TimeOutSeconds?:  number;
+
+    constructor(method: string, queryString: string, data?:string){
+        this.Method = method;
+        this.QueryString = queryString;
+        this.Data = data;
+    }
+ 
+ }
+
+ export class ContainerSetup {
+    public WidgetId?: number;
+    public ContainerId: string;
+    public ContractedHeight: number;
+    public ExpandedHeight: number;
+    public HasSetup: boolean;
+    public SupportEmail: string;
+    public SupportTel: string;
+    public SupportUrl: string;
+
+    constructor(WidgetId: number, ContainerId: string, ContractedHeight: number, ExpandedHeight: number, HasSetup: boolean, SupportEmail: string, SupportTel: string, SupportUrl: string){
+        this.WidgetId = WidgetId;
+        this.ContainerId = ContainerId;
+        this.ContractedHeight = ContractedHeight;
+        this.ExpandedHeight = ExpandedHeight;
+        this.HasSetup = HasSetup;
+        this.SupportEmail = SupportEmail;
+        this.SupportTel = SupportTel;
+        this.SupportUrl = SupportUrl;
+    }
+}
